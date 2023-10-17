704d68d347c841fd3c6cab8157abf0d489e00380
luaanHD <luantoniolo94@gmail.com>
2023-10-17 15:15:41 +0000

    Expressão Regular

---
+++ ClasseExpressaoRegular2.java
@@ -0,0 +1,15 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ClasseExpressaoRegular2 {
+
+    public static void main(String[] args) {
+
+        Pattern p = Pattern.compile("((hi|su)per)mercado");
+
+        Matcher n = p.matcher("supermercado");
+        while (n.find()) {
+            System.out.println(n.group());
+        }
+    }
+}
--- ClasseExpresseRegular.java
+++ ClasseExpresseRegular.java
@@ -4,8 +4,8 @@ import java.util.regex.Pattern;
 public class ClasseExpresseRegular {
     
     public static void main(String[] args) {
-        Pattern p =Pattern.compile("[0-9]{3}");
-        Matcher n = p.matcher("123ABC");
+        Pattern p =Pattern.compile("[0-9]{3}[A-Z]{3}");
+        Matcher n = p.matcher("123ABC456");
         while(n.find()){
             System.out.println(n.group());
         }
